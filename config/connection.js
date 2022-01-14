f69bedbae3292bc36b6f5fef60ce9ed3bf6aadbb
Caswell Orr <caswell.orr@gmail.com>
2022-01-14 13:36:33 -0600

    maybe this time

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,11 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
 
-connect(connectionString, 
+connect(connectionString || 'mongodb://localhost/infinite-shore', 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
 );
-
-// || 'mongodb://localhost/infinite-shore'
 
 module.exports = connection;
